f456fe9c056c3acc701de0e4ae600b6bdbad4ecc
akif <su92-bssem-f22-118@superior.edu.pk>
2025-03-20 23:03:21 +0500

    edit user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,38 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Sign } from 'crypto';
 import { Signup, SignupSchema } from './user.schema';
-
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Sign.name, schema: SignupSchema }])],
+  imports: [MongooseModule.forFeatureAsync([{ name: Signup.name,  useFactory
+    : () => {
+    const schema = SignupSchema;
+    schema.pre('save', function (next) {
+      const user=this as any;
+      if(!user.name){
+        return next(new Error('Title must be at least 3 characters long.'));
+      }
+      if (!user.email ) {
+        return next(new Error('Description must be at least 10 characters long.'));
+      }
+      if (!user.password||user.password<10||user.password>10) {
+        return next(new Error('password must be at least 10 characters long.'));
+      }
+      if (!user.password!==user.confirmPassword) {
+        return next(new Error('password must be at least 10 characters long.'));
+      }    
+      if (!user.role) {
+        return next(new Error('role must be required'));
+      }
+      if (!user.age||user.age<18) {
+        return next(new Error('age must be required and greater than 18'));
+      }
+      if (!user.education) {
+        return next(new Error('age must be required and greater than 18'));
+      }
+      console.log('Task validation passed ');
+      next();
+    });
+    return schema;
+  } }])],
   controllers: [UserController],
   providers: [UserService]
 })
--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,30 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-
 export type SignupDocument = HydratedDocument<Signup>;
-
 @Schema()
 export class Signup {   
   @Prop({ required: true })
   name: string;
-
   @Prop({ required: true, unique: true })
   email: string;
-
   @Prop({ required: true })
   password: string;
-
   @Prop({ required: true })
   confirmPassword: string;
-
   @Prop({ required: true })
+
   role: string;
-
   @Prop({ required: true })
-  age: string;
 
+  age: string;
   @Prop({ required: true })
   education: string;
+  @Prop()
+  image: string;
 }
-
 export const SignupSchema = SchemaFactory.createForClass(Signup);
