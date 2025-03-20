7f0ae8d35a7bf7e6accde844f28edb5ed5f4e663
akif <su92-bssem-f22-118@superior.edu.pk>
2025-03-20 21:14:30 +0500

    add modle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [TaskModule, UserModule,
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
